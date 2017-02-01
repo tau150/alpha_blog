716ab9b388eb04df5d9e1a2122b698a403255982
Santi Nuñez <Santi@MacBook-Santi.local>
2017-02-01 19:29:05 -0300

    users validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,12 @@
 class User < ActiveRecord::Base
 
-  
+  validates :username, presence: true, 
+            uniqueness: { case_sensitive: false},
+            length: { minumun: 3, maximum: 25}
 
+ VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i 
+ validate :email, presence:true,
+          length: { maximum: 105}
+          uniqueness: { case_sensitive: false},
+          format: { with: VALID_EMAIL_REGEX}
 end
