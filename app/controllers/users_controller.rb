b6439e2d5f696238908025d54cde2bc637b14608
Santi Nuñez <Santi@MacBook-Santi.local>
2017-02-12 09:45:46 -0300

    add sign in upon sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,8 +16,9 @@ before_action :require_same_user, only: [:edit, :update]
     def create
       @user = User.new(user_params)
       if @user.save
+        session[:user_id] = @user.id
         flash[:success] = "welcome to the alpha blog #{@user.username}"
-        redirect_to articles_path
+        redirect_to user_path(@user)
         else
           render 'new'
       end
