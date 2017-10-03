03d685f223324b4680911eda6b6951761fea596a
Santi Nuñez <tau150@hotmail.com>
2017-10-03 10:40:27 -0300

    add article and category association

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,9 @@
 class Article < ActiveRecord::Base
   belongs_to :user
-  validates :title, presence: true, length: { minimum: 3 , maximum: 50}
-  validates :description, presence:true, length: { minimum: 10 , maximum: 300}
+  validates :title, presence: true, length: { minimum: 3, maximum: 50 }
+  validates :description, presence: true, length: { minimum: 10, maximum: 300 }
   validates :user_id, presence: true
+
+  has_many :article_categories
+  has_many :categories, through: :article_categories
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170927122606) do
+ActiveRecord::Schema.define(version: 20171003130831) do
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
